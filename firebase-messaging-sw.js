81e9b9e98e2ff7b9a5eec9af767db8a86236b706
pablosantamaria26 <santamariapablodaniel@gmail.com>
2025-10-31 12:59:03 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,23 +2,21 @@
 importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js');
 
-// Configuración de tu proyecto Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyAKEZoMaPwAcLVRFVPVTQEOoQUuEEUHpwk",
   authDomain: "app-vendedores-inteligente.firebaseapp.com",
   projectId: "app-vendedores-inteligente",
-  storageBucket: "app-vendedores-inteligente.firebasestorage.app",
+  storageBucket: "app-vendedores-inteligente.appspot.com",
   messagingSenderId: "583313989429",
   appId: "1:583313989429:web:c4f78617ad957c3b11367c"
 });
 
 const messaging = firebase.messaging();
 
-// Cuando llega una notificación con la app cerrada o en segundo plano
-messaging.onBackgroundMessage((payload) => {
-  console.log('📩 Notificación recibida en segundo plano:', payload);
+messaging.onBackgroundMessage(function(payload) {
+  console.log("📩 Notificación en segundo plano:", payload);
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: '/icon-192.png' // Ícono de tu app
+    icon: "/ml-icon-192.png"
   });
 });
