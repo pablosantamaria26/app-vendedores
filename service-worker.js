8fbf8322a0857c666a1d8015a367c31ad6f65026
pablosantamaria26 <santamariapablodaniel@gmail.com>
2026-02-03 16:37:17 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* service-worker.js — Mercado Limpio Ventas (PWA) */
-const CACHE_VERSION = 'ml-pwa-v7';
+const CACHE_VERSION = 'ml-pwa-v8';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
